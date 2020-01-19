fcc2c9c4e160f689eb4a15e988eaf4f4e75caa07
gaoyang <30515848+gaoyang23nj@users.noreply.github.com>
2020-01-19 23:12:14 +0800

    Update Test_ML.py
    train blackhole

--- Test/Test_ML.py
+++ Test/Test_ML.py
@@ -15,19 +15,9 @@ def process_data_npz(file_path, data_srcnode):
     x_ind = data['x_ind']
     x_ind_new = np.delete(x_ind, data_srcnode, axis = 0)
     return y_new, x_d_new, x_ind_new
-    # print('{},{}'.format(y.shape, y_new.shape))
-    # print('{},{}'.format(x_d.shape, x_d_new.shape))
-    # print('{},{}'.format(x_ind.shape, x_ind_new.shape))
-    # print('before:')
-    # print(y)
-    # print(x_d)
-    # print(x_ind)
-    # print('after:')
-    # print(y_new)
-    # print(x_d_new)
-    # print(x_ind_new)
 
-if __name__ == "__main__":
+# 从直接证据里 训练分类器
+def train_from_DirectEvidence():
     dir = "../Main/collect_data/scenario9/"
     files = os.listdir(dir)
     file_path = ""
@@ -61,5 +51,47 @@ if __name__ == "__main__":
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
     model.fit(X_train, y_train, epochs=5)
-
     model.evaluate(X_test, y_test, verbose=2)
+    print('num_train: {}, {}; num_test, {}, {};'.format(y_train.shape, np.sum(y_train), y_test.shape, np.sum(y_test)))
+
+# 从间接证据里 训练分类器
+def train_from_inDirectEvidence():
+    dir = "../Main/collect_data/scenario9/"
+    files = os.listdir(dir)
+    file_path = ""
+    data_srcnode = 0
+    # 合计有100个数据来源
+    # y.shape 99*1
+    y_final = np.zeros(shape=(99 * 100,))
+    # x1.shape 99*3
+    x1_final = np.zeros(shape=(99 * 100, 3))
+    # x2.shape 99*300
+    x2_final = np.zeros(shape=(99 * 100, 300))
+    for file in files:
+        file_path = os.path.join(dir, file)
+        data_srcnode = int(file.split('.')[0])
+        # 收集所有data 训练模型
+        y, x1, x2 = process_data_npz(file_path, data_srcnode)
+        y_final[data_srcnode * 99 : (data_srcnode+1) * 99] = y
+        x1_final[data_srcnode * 99: (data_srcnode + 1) * 99] = x1
+        x2_final[data_srcnode * 99: (data_srcnode + 1) * 99] = x2
+    # 只利用x1属性
+    X_train, X_test, y_train, y_test = train_test_split(x2_final, y_final, test_size = 0.3)
+    min_max_scaler = MinMaxScaler()
+    X_train = min_max_scaler.fit_transform(X_train)
+    X_test = min_max_scaler.transform(X_test)
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(300, activation='relu',input_shape=[300,]),
+        tf.keras.layers.Dense(300, activation='relu'),
+        tf.keras.layers.Dense(2, activation='softmax')
+    ])
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.fit(X_train, y_train, epochs=5)
+    model.evaluate(X_test, y_test, verbose=2)
+    print('num_train: {}, {}; num_test, {}, {};'.format(y_train.shape, np.sum(y_train), y_test.shape, np.sum(y_test)))
+
+if __name__ == "__main__":
+    train_from_DirectEvidence()
+    # train_from_inDirectEvidence()
